23a41be17b950394bf33b92f0b4a14b9c83d0fb8
PuzatykhMikhail <puzatyh.mihail@yandex.ru>
2023-09-21 12:10:53 +0300

    fixing comments
    fixing comments

--- src/dags/sprint3.py
+++ src/dags/sprint3.py
@@ -2,10 +2,11 @@ import time
 import requests
 import json
 import pandas as pd
+import logging
 
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.hooks.http_hook import HttpHook
@@ -14,14 +15,16 @@ http_conn_id = HttpHook.get_connection('http_conn_id')
 api_key = http_conn_id.extra_dejson.get('api_key')
 base_url = http_conn_id.host
 
-postgres_conn_id = 'postgresql_de'
+tsk_logger = logging.getLogger('airflow.task')
 
-nickname = 'mikhail_puzatykh'
-cohort = '17'
+POSTGRES_CONN_ID = 'postgresql_de'
+
+NICKNAME = 'mikhail_puzatykh'
+COHORT = '17'
 
 headers = {
-    'X-Nickname': nickname,
-    'X-Cohort': cohort,
+    'X-Nickname': NICKNAME,
+    'X-Cohort': COHORT,
     'X-Project': 'True',
     'X-API-KEY': api_key,
     'Content-Type': 'application/x-www-form-urlencoded'
@@ -29,7 +32,7 @@ headers = {
 
 
 def generate_report(ti):
-    print('Making request generate_report')
+    tsk_logger.info('Making request generate_report')
 
     response = requests.post(f'{base_url}/generate_report', headers=headers)
     response.raise_for_status()
@@ -39,7 +42,7 @@ def generate_report(ti):
 
 
 def get_report(ti):
-    print('Making request get_report')
+    tsk_logger.info('Making request get_report')
     task_id = ti.xcom_pull(key='task_id')
 
     report_id = None
@@ -56,14 +59,14 @@ def get_report(ti):
             time.sleep(10)
 
     if not report_id:
-        raise TimeoutError()
+        raise TimeoutError('ID report not received')
 
     ti.xcom_push(key='report_id', value=report_id)
     print(f'Report_id={report_id}')
 
 
 def get_increment(date, ti):
-    print('Making request get_increment')
+    tsk_logger.info('Making request get_increment')
     report_id = ti.xcom_pull(key='report_id')
     response = requests.get(
         f'{base_url}/get_increment?report_id={report_id}&date={str(date)}T00:00:00',
@@ -81,7 +84,7 @@ def get_increment(date, ti):
 
 def upload_data_to_staging(filename, date, pg_table, pg_schema, ti):
     increment_id = ti.xcom_pull(key='increment_id')
-    s3_filename = f'https://storage.yandexcloud.net/s3-sprint3/cohort_{cohort}/{nickname}/project/{increment_id}/{filename}'
+    s3_filename = f'https://storage.yandexcloud.net/s3-sprint3/cohort_{COHORT}/{NICKNAME}/project/{increment_id}/{filename}'
     print(s3_filename)
     local_filename = date.replace('-', '') + '_' + filename
     print(local_filename)
@@ -91,17 +94,17 @@ def upload_data_to_staging(filename, date, pg_table, pg_schema, ti):
     print(response.content)
     #Delete for 3 task in project------------------------------------
     del_query = f"delete from {pg_schema}.{pg_table} where date_time::date = '{date}'"
-    postgres_hook = PostgresHook(postgres_conn_id)
+    postgres_hook = PostgresHook(POSTGRES_CONN_ID)
     postgres_hook.run(sql=del_query)
     #-----------------------------------------------------------------------
-    df = pd.read_csv(local_filename)
-    df=df.drop('id', axis=1)
+    df = pd.read_csv(local_filename, index_col=0)
+    # df=df.drop('id', axis=1)
     df=df.drop_duplicates(subset=['uniq_id'])
 
     if 'status' not in df.columns:
         df['status'] = 'shipped'
 
-    postgres_hook = PostgresHook(postgres_conn_id)
+    postgres_hook = PostgresHook(POSTGRES_CONN_ID)
     engine = postgres_hook.get_sqlalchemy_engine()
     row_count = df.to_sql(pg_table, engine, schema=pg_schema, if_exists='append', index=False)
     print(f'{row_count} rows was inserted')
@@ -112,7 +115,7 @@ args = {
     'email': ['student@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 0
+    'retries': 5
 }
 
 business_dt = '{{ ds }}'
@@ -145,32 +148,33 @@ with DAG(
                    'filename': 'user_order_log_inc.csv',
                    'pg_table': 'user_order_log',
                    'pg_schema': 'staging'})
+    dimension_tsk = []
+    for dimension_tab in ['d_item', 'd_customer', 'd_city']:
+        dimension_tsk.append( PostgresOperator(
+            task_id=f'update_{dimension_tab}',
+            postgres_conn_id=POSTGRES_CONN_ID,
+            sql=f'sql/mart.{dimension_tab}.sql'))
+                            
+    # update_d_customer_table = PostgresOperator(
+    #     task_id='update_d_customer',
+    #     postgres_conn_id=POSTGRES_CONN_ID,
+    #     sql="sql/mart.d_customer.sql")
 
-    update_d_item_table = PostgresOperator(
-        task_id='update_d_item',
-        postgres_conn_id=postgres_conn_id,
-        sql="sql/mart.d_item.sql")
-
-    update_d_customer_table = PostgresOperator(
-        task_id='update_d_customer',
-        postgres_conn_id=postgres_conn_id,
-        sql="sql/mart.d_customer.sql")
-
-    update_d_city_table = PostgresOperator(
-        task_id='update_d_city',
-        postgres_conn_id=postgres_conn_id,
-        sql="sql/mart.d_city.sql")
+    # update_d_city_table = PostgresOperator(
+    #     task_id='update_d_city',
+    #     postgres_conn_id=POSTGRES_CONN_ID,
+    #     sql="sql/mart.d_city.sql")
 
     update_f_sales = PostgresOperator(
         task_id='update_f_sales',
-        postgres_conn_id=postgres_conn_id,
+        postgres_conn_id=POSTGRES_CONN_ID,
         sql="sql/update_f_sales.sql",
         parameters={"date": {business_dt}}
     )
 
     update_f_customer_retention = PostgresOperator(
         task_id='update_f_customer_retention',
-        postgres_conn_id=postgres_conn_id,
+        postgres_conn_id=POSTGRES_CONN_ID,
         sql="sql/update_f_customer_retention.sql",
         parameters={"date": {business_dt}}
     )
@@ -180,7 +184,7 @@ with DAG(
             >> get_report
             >> get_increment
             >> upload_user_order_inc
-            >> [update_d_item_table, update_d_city_table, update_d_customer_table]
+            >> dimension_tsk
             >> update_f_sales
             >> update_f_customer_retention
     )
